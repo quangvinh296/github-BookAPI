b720c7d9a9e40de108bce27268220a0434c470f2
quangvinh296 <quangvinh296@gmail.com>
2021-01-04 12:19:13 +0700

    Upload Book API

---
+++ BookAPI/src/test/java/demo/StringBoot/BookAPI/BookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.StringBoot.BookAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
